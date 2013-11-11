779c0c7529b9ad7b940750f5191a657ea3d7bb46
flux3000 <ryanfbaker@gmail.com>
2013-11-10 16:42:04 -0800

    Now pulling back and sorting 40 results. Radius filter not working as expected

--- _js/script.js
+++ _js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	init();
  
     /////////// Geolocation stuff ///////////
-    /*
+    
     if (navigator.geolocation) {
         var options = {
           enableHighAccuracy: true,
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
           $("#mylatlong").html(mylat + ", " + mylong);
 
-          //google.maps.event.addDomListener(window, 'load', mapsInitialize(mylatlong[0], mylatlong[1], "map-me"));    
+          google.maps.event.addDomListener(window, 'load', mapsInitialize(mylatlong[0], mylatlong[1], "map-canvas"));    
         
         };
 
@@ -27,7 +27,7 @@ $(document).ready(function() {
         };       
 
         navigator.geolocation.getCurrentPosition(success,error,options);       
-    } */ 
+    }  
 
     // Get Yelp Data
 	$('.btn-primary').click(function() {
@@ -47,7 +47,11 @@ $(document).ready(function() {
 
 	    var testlat = 37.8621574;
 	    var testlong = -122.25017369999999;
+/*	    var testlat = 37.7697;
+	    var testlong = -122.4769;*/
 	    var distance = 4828; // 3 miles
+	    //var distance = 30000; // testing
+	    //var location = "2816 Derby Street Berkeley, CA 94705"
 
 		var accessor = {
 		  consumerSecret: auth.consumerSecret,
@@ -57,9 +61,11 @@ $(document).ready(function() {
 		parameters = [];
 
 		parameters.push(['ll', testlat+','+testlong]);
-		parameters.push(['radius_filter', 4828]);
+		//parameters.push(['location', location]);
+
+		parameters.push(['radius_filter', distance]);
 		parameters.push(['category_filter', "restaurants"]);
-		parameters.push(['sort', 1]);
+		parameters.push(['sort', 2]);
 
 		parameters.push(['callback', 'cb']);
 		parameters.push(['oauth_consumer_key', auth.consumerKey]);
@@ -73,7 +79,7 @@ $(document).ready(function() {
 		  'parameters': parameters 
 		};
 		var firstparameters = parameters;
-		console.log(firstparameters);
+		//console.log(firstparameters);
 
 		OAuth.setTimestampAndNonce(message);
 		OAuth.SignatureMethod.sign(message, accessor);
@@ -85,40 +91,35 @@ $(document).ready(function() {
 		  'url': message.action,
 		  'data': parameterMap,
 		  'cache': true,
+		  'async': true,
 		  'dataType': 'jsonp',
 		  'jsonpCallback': 'cb',
 		  'success': function(data, textStats, XMLHttpRequest) {
-		    //console.log(data);
-		    returnResults(data);
-		  }
-	    });
 
-		parameters.push(['offset', 20]);
-		parameters.push(['limit', 20]);
+		    // prepare the second call, for the next 20 results.
+			parameters.push(['offset', 20]);
+			parameters.push(['limit', 20]);
 
-		var message = { 
-		  'action': 'http://api.yelp.com/v2/search',
-		  'method': 'GET',
-		  'parameters': parameters 
-		};
-		var secondparameters = parameters;
-		console.log(secondparameters);
+			OAuth.setTimestampAndNonce(message);
+			OAuth.SignatureMethod.sign(message, accessor);
 
-		OAuth.setTimestampAndNonce(message);
-		OAuth.SignatureMethod.sign(message, accessor);
+			var parameterMap = OAuth.getParameterMap(message.parameters);
+			parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
 
-		var parameterMap = OAuth.getParameterMap(message.parameters);
-		parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+			$.ajax({
+			  'url': message.action,
+			  'data': parameterMap,
+			  'cache': true,
+			  'async': true,
+			  'dataType': 'jsonp',
+			  'jsonpCallback': 'cb',
+			  'success': function(newdata, textStats, XMLHttpRequest) {
+			    var mybusinesses = $.merge(data.businesses, newdata.businesses);
+			    //console.log(mybusinesses);			    
+			    returnBusinesses(mybusinesses);
 
-		$.ajax({
-		  'url': message.action,
-		  'data': parameterMap,
-		  'cache': true,
-		  'dataType': 'jsonp',
-		  'jsonpCallback': 'cb',
-		  'success': function(data, textStats, XMLHttpRequest) {
-		    //console.log(data);
-		    returnResults(data);
+			  }
+		    });
 		  }
 	    });
 
@@ -126,32 +127,55 @@ $(document).ready(function() {
 
 });
 
-function returnResults(data) {
-
-	var myresults = [];
-
-	if(!data.businesses.length==0) {
-		for (var i = 0; i < data.businesses.length; i++) {
-			var this_result = [];
-			this_result["name"] = data.businesses[i]["name"];
-		    this_result["rating"] = data.businesses[i]["rating"];
-		    this_result["distance"] = ((data.businesses[i]["distance"]) * 0.000621371).toFixed(2);
-		    myresults.push(this_result);
-		}
-
-		// sort results by distance, lowest first
-		myresults.sort(function(a,b) {
-		  return parseFloat(a.distance,10) - parseFloat(b.distance,10);
-		});
-		console.log(myresults);
-
-		for (var j = 0; j < myresults.length; j++) {
-			$("#business-results").append('<li>'+(j+1)+'. '+myresults[j]["name"]+' - Distance: '+myresults[j]["distance"]+' Miles - Avg Rating: '+myresults[j]["rating"]+'</li>');
-		}
-
-	}
-}
-
 function init() {
 
 }
+
+function returnBusinesses(businesses) {
+	//console.log(businesses);
+	var myresults = [];
+	if(!businesses.length==0) {
+		for (var i = 0; i < businesses.length; i++) {
+			var this_result = [];
+			this_result["name"] = businesses[i]["name"];
+		    this_result["rating"] = businesses[i]["rating"];
+		    this_result["distance"] = ((businesses[i]["distance"]) * 0.000621371).toFixed(2);
+		    myresults.push(this_result);
+		}
+	}
+	showBusinesses(myresults);
+}
+
+function showBusinesses(myresults){
+	// sort results by distance, lowest first
+	myresults.sort(function(a,b) {
+	  return parseFloat(a.distance,10) - parseFloat(b.distance,10);
+	});
+
+	for (var j = 0; j < myresults.length; j++) {
+		$("#business-results").append('<li>'+(j+1)+'. '+myresults[j]["name"]+' - Distance: '+myresults[j]["distance"]+' Miles - Avg Rating: '+myresults[j]["rating"]+'</li>');
+	}
+	$("#business-results").append('<li>-----------------</li>');	
+}
+
+
+// google maps code
+var map;
+function mapsInitialize(lat, long, targetID) {
+    var myLatlng = new google.maps.LatLng(lat, long);
+    
+    var mapOptions = {
+    	zoom: 14,
+    	center: myLatlng,
+    	mapTypeId: google.maps.MapTypeId.ROADMAP   
+    };
+    
+    map = new google.maps.Map(document.getElementById(targetID), mapOptions);
+    
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Hello World!'
+    });
+}
+
